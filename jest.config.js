f3888e7d718af9f34ae4db7ce3b8442c797836c4
Wannes Salomé <mail@wannessalome.nl>
2020-02-15 23:37:56 +0100

    Add tsconfig path mapping

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
 };
