e878eb1a1dab0c80277232678479865f7e53b4ee
Wannes Salomé <mail@wannessalome.nl>
2020-02-15 23:37:41 +0100

    Add custom rules and typescript import resolver

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ module.exports = {
   // Add custom rules here
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
     'import/no-extraneous-dependencies': [
       'error',
       { devDependencies: ['**/*.test.tsx', 'test/**/*'] },
     ],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'no-useless-constructor': 'off',
     quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
   },
 };
